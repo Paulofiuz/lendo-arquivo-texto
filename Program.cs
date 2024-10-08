02a1b80805dfb3d2dfde202df3bad6adc8c032a4
EderPaulo <paulofiuza24@gmail.com>
2024-10-08 18:49:32 -0300

    Lendo arquivo .txt

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+namespace LerArquivos
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+
+            string caminhoArquivo = "C:\\dev\\LerArquivos\\meuArquivo.txt";
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(caminhoArquivo))
+                {
+                    string linha;
+                    while ((linha = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(linha);
+
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Erro ao ler o arquivo: " + e.Message);
+            }
+        }
+    }
+}
